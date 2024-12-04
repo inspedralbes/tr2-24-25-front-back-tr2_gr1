e92870ac9b8bf1dc24bced2cdf6c0ac1d63c9011
Arnau-fg <a22arnfergil@inspedralbes.com>
2024-12-04 09:21:14 +0100

    Tailwind instal·lat

---
+++ front/fronttr2/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  darkMode: false, 
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  content: [
+    // Example content paths...
+    './public/**/*.html',
+    './src/**/*.{js,jsx,ts,tsx,vue}',
+  ],
+}
+
