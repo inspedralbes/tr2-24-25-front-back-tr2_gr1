db633a5cf08b71c6143217d85e897d6e088578ee
davmungon <a23davmungon@inspedralbes.cat>
2024-12-09 09:58:51 +0100

    Panatall login

---
+++ front/fronttr2/src/components/Login.vue
@@ -0,0 +1,85 @@
+<template>
+    <div class="screen-container">
+        <Card>
+            <template #title>Iniciar sesió</template>
+            <template #content>
+                <div class="card flex flex-wrap justify-center items-end gap-4">
+                    <FloatLabel variant="in">
+                        <label for="correu_label">Correu</label>
+                        <InputText id="correu_label" v-model="correu" autocomplete="off" />
+                    </FloatLabel>
+                </div>
+                <div class="card flex flex-wrap justify-center items-end gap-4">
+                    <FloatLabel variant="in">
+                        <label for="contrasenya_label">Contrasenya</label>
+                        <InputText type="password" id="contrasenya_label" v-model="contrasenya" autocomplete="off" />
+                    </FloatLabel>
+                </div>
+                <div class="card flex flex-wrap justify-center items-end gap-4">
+                    <Button label="Iniciar sesión" icon="pi pi-check" iconPos="left" severity="success" :loading="loading" @click="login" />
+                </div>
+                <Dialog :visible="visible" modal header="Alerta" :style="{ width: '25rem' }" class="bg-white">
+                    <span class="text-surface-500 dark:text-surface-400 block mb-8">Es requereix un Correu i una Contrasenya</span>
+                    <div class="flex justify-end gap-2">
+                        <Button type="button" label="Entesos" @click="visible = false" severity="success"></Button>
+                    </div>
+                </Dialog>
+            </template>
+        </Card>
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import InputText from 'primevue/inputtext';
+import FloatLabel from 'primevue/floatlabel';
+import Button from 'primevue/button';
+import Dialog from 'primevue/dialog';
+import Card from 'primevue/card';
+import { loginUsuari } from './../services/comunicationManager';
+
+const correu = ref(null);
+const contrasenya = ref(null);
+const loading = ref(false);
+var visible = ref(false);
+
+async function login() {
+    if (!correu.value || !contrasenya.value) {
+        visible.value = true;
+        return;
+    }
+    loading.value = true;
+    try {
+        const result = await loginUsuari(correu.value, contrasenya.value);
+        if (result) {
+            alert('Inici de sessió exitós!');
+        } else {
+            alert('Correu o contrasenya incorrectes');
+        }
+    } catch (error) {
+        console.error('Error al iniciar sesión:', error);
+        alert('Hi ha hagut un error en iniciar la sessió.');
+    } finally {
+        loading.value = false;
+    }
+}
+</script>
+
+<style scoped>
+.screen-container {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 5px;
+    height: 100vh;
+    padding: 10px;
+    background-color: #f8f9fa;
+}
+
+.card {
+    padding: 10px;
+    max-width: 400px;
+    width: 100%;
+}
+</style>
