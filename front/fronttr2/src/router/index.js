9b45cee9198f1328877d278ef2759ef94d355a88
davmungon <a23davmungon@inspedralbes.cat>
2024-12-09 10:11:28 +0100

    Ruta pantalla login

--- front/fronttr2/src/router/index.js
+++ front/fronttr2/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import NewAssociacio from '../views/NewAssociacioView.vue'
+import Login from '@/components/Login.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -37,6 +38,11 @@ const router = createRouter({
       path: '/show',
       name: 'show',
       component: () => import('../views/ShowAssociations.vue'),
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login
     }
   ],
 });
