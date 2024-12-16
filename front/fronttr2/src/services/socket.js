9fe2ebd6d6dacc3aa692b100871a247f20a24668
Arnau Fernandez Gil <145099360+Arnau-fg@users.noreply.github.com>
2024-12-16 11:00:25 +0100

    merged pinia with chat

--- front/fronttr2/src/services/socket.js
+++ front/fronttr2/src/services/socket.js
@@ -1,14 +1,19 @@
 import { io } from "socket.io-client";
+import { useChatStore } from '../stores/chat';
 
+const chatStore = useChatStore();
 
 const URL = "http://localhost:3001/";
 
 export const socket = io(URL);
 
+
 socket.on("allMessages", (data)=>{
     console.log(data);
+    chatStore.setMessages(data);
 })
 
 socket.on("chat message", (data)=>{
     console.log(data);
+    chatStore.addMessage(data);
 })
