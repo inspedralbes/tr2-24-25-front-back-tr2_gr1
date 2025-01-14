6e613caa520a25605f5144190126980ffeb9fbf8
Fabianroldan <a21fabrolfer@inspedralbes.cat>
2025-01-14 10:23:30 +0100

    create noticies done

--- front/fronttr2/src/services/comunicationManager.js
+++ front/fronttr2/src/services/comunicationManager.js
@@ -461,6 +461,7 @@ export const createNoticia = async ({ titol, subtitol, contingut, imatge, autor,
         } else {
             token = user.token;
         }
+        console.log("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA ----> ", titol, subtitol, contingut, imatge, autor, idAsso);
         const response = await fetch(`${URLNOTICIAS}/api/noticia`, {
             method: 'POST',
             headers: {
@@ -473,6 +474,7 @@ export const createNoticia = async ({ titol, subtitol, contingut, imatge, autor,
             const data = await response.json();
             console.log('Noticia created successfully:', data);
         } else if (response.status === 400) {
+            console.log("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA ----> ", titol, subtitol, contingut, imatge, autor, idAsso);
             console.error('Invalid input');
         } else if (response.status === 401) {
             noLogged();
